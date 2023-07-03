761dd83aa4121dffd35844d233edb9b62c63cdf2
Ruan Carolino Segrini <102704092+RuanSegrini@users.noreply.github.com>
2023-07-03 11:41:37 -0300

    Update Notas.java

--- EXERCISES-JAVA/CONDITIONAL STRUCTURE/Notas.java
+++ EXERCISES-JAVA/CONDITIONAL STRUCTURE/Notas.java
@@ -1,1 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
 
+public class App {
+public static void main(String[] args) {
+ Locale.setDefault(Locale.US);
+ Scanner sc = new Scanner(System.in);
+
+ double nota1, nota2, notaFinal;
+ 
+  System.out.print("Digite a primeira nota: ");
+  nota1 = sc.nextDouble();
+  System.out.print("Digite a segunda nota: ");
+  nota2 = sc.nextDouble();
+
+  notaFinal = nota1 + nota2;
+
+  if(notaFinal < 60){
+    System.out.print("REPROVADO");
+  }
+  else{
+
+ System.out.println("NOTA FINAL: " + String.format("%.1f", notaFinal));
+  } 
+
+ 
+ sc.close();
+ }
+} 
